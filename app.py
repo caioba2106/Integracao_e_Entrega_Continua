059fe4133150dcb606bac8820fd3a1c9ad2b44cd
CAIO BERTOCHI RODRIGUES <1050292211021@fatec>
2026-03-30 10:42:20 -0300

    corrigindo identação

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-
-@app.route("/sobre")
-def sobre():
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def minha_funcao():
+    print("teste")
--- main.py
+++ main.py
@@ -17,3 +17,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 
+
+
+
